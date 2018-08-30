a40d08c5666135a1a388c21f4d4a8113658c562a
Adrian Gonzalez <adrianandgonzalez@gmail.com>
2018-08-30 10:46:58 -0700

    created card deck with shuffle functionality and ability to write and read from os

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	// cards := shuffle(newDeck())
+	// hand, remainingDeck := deal(cards, 5)
+
+	// fmt.Println("Hand")
+	// hand.print()
+
+	// fmt.Println("remainingDeck")
+	// remainingDeck.print()
+
+	// cards := newDeck()
+	cards := newDeckFromFile("myCards")
+
+	fmt.Println(cards)
 
 }
